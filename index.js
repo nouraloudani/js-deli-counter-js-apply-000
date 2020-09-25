faea178f2288d2b717415fcffadbddf4a57ef691
Noura Loudani <contact.nouraloudani@gmail.com>
2020-09-25 02:29:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function nowServing(currentLine) {
 
 function currentLine(line) {
    let nowInLine=[''];
-    return nowInline[i].length + nowInline[i]
+    return nowInline[i+1] + nowInline[i]
     
 }
