fbed5fa7824e011b8bee7215917f712431a5f840
Noura Loudani <contact.nouraloudani@gmail.com>
2020-09-25 02:14:31 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function takeANumber(currentLine, newPerson) {
+  
+  currentLine.push(newPerson); //[] -> ["Ada"]
+  return "Welcome, " + newPerson + ". You are number " + currentLine.length + " in line.";
+  
+}
+const currentLine = [];
+takeANumber(currentLine, "Ada");
+
+function nowServing(currentLine) {
+  
+  if (currentLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  
+  let firstPersonInLine = currentLine.shift();
+    return "Currently serving " + firstPersonInLine + '.';
+}
+
+function currentLine()
